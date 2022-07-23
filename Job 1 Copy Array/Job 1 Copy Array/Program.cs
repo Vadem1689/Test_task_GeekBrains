0af7ff79673284e68768d0ab00f96826a379f7aa
fuyfuy8p8 <yakimovvadem@gmail.com>
2022-07-23 15:19:48 +0300

    GeekBrains  is the best school

---
+++ Job 1 Copy Array/Job 1 Copy Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_1_Copy_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array1 = { 0, 1, 2, 3 };
+            int[] array2 = new int[4];
+
+            //ВАРИАНТ 1
+
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    array2[i] = array1[i];
+            //    Console.WriteLine(array2[i]);
+            //}
+
+
+            //ВАРИАНТ 2
+
+            Array.Copy(array1, array2, 4);
+
+            foreach (var element in array2)
+            {
+                Console.WriteLine(element);
+            }
+
+
+
+        }
+    }
+}
---
+++ Task 2 product of arrays/Task 2 product of arrays/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_task_GeekBrains
+{
+
+	using System;
+
+	class Program
+	{
+		//Программа определяет произведение массивов любой длинны и степени
+
+		static int[] multiply(int[] array1, int[] array2,
+								int size1, int size2)
+		{
+			int[] prod = new int[size1 + size2 - 1];
+
+			for (int i = 0; i < size1 + size2 - 1; i++)
+			{
+				prod[i] = 0;
+			}
+
+
+			for (int i = 0; i < size1; i++)
+			{
+
+				for (int j = 0; j < size2; j++)
+				{
+					prod[i + j] += array1[i] * array2[j];
+				}
+			}
+
+			return prod;
+		}
+
+
+		static void White(int[] poly, int n)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				Console.Write(poly[i]);
+				if (i != 0)
+				{
+					Console.Write("x^" + i);
+				}
+				if (i != n - 1)
+				{
+					Console.Write(" + ");
+				}
+			}
+			Console.WriteLine();
+		}
+
+
+		public static void Main(String[] args)
+		{
+
+			int[] array1 = { 8, 1 };  /*Здесь можно задать любой массив*/
+			int[] array2 = { 4, 2 }; /*Здесь можно задать любой массив*/
+			int size1 = array1.Length;
+			int size2 = array2.Length;
+
+			Console.Write("Первый многочлен:  ");
+			White(array1, size1);
+			Console.Write("Второй многочлен:  ");
+			White(array2, size2);
+
+			int[] prod = multiply(array1, array2, size1, size2);
+
+			Console.Write("Произведеие:  ");
+			White(prod, size1 + size2 - 1);
+
+
+		}
+	}
+
+
+}
